6c1d80fd93d547f0db4b94e22c0c74bc43fdc764
SebastienBocquet <sbocquet@gmail.com>
2018-10-04 17:20:05 +0200

    Revert "Suppression du mecanisme hover_counter"
    This reverts commit 7d9eb163aa38d4d0825000f09117a1f06ef49ef3.

--- MatrixPilot/defines.h
+++ MatrixPilot/defines.h
@@ -140,10 +140,12 @@ extern int16_t lidar_height_to_ground ;		// calculated distance to ground in cm
 #if (SILSIM == 1)
 	extern long barometer_altitude;
 	extern int16_t nb_sample_wait;
+    extern int16_t hover_counter;
 	extern int16_t z_target;
 	extern int16_t vz_target;
 #endif
 
+extern int16_t hover_counter;
 extern int16_t z_filtered;
 extern int16_t rampe_throttle;
 extern int16_t hover_target_z;
